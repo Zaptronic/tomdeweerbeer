d62c9c72a21fbf8b533b63c5850f1ceddaa518de
Zaptronic <info@zaptronic.nl>
2016-02-11 20:59:39 +0100

    onpause

--- doumentready.js
+++ doumentready.js
@@ -1,6 +1,6 @@
 document.addEventListener("deviceready", function(){
 //    navigator.geolocation.getCurrentPosition(currentlocationtocurrentcity, currentlocationerror, { timeout: 30000 });
-//    document.addEventListener("pause", onPause, false);
+    document.addEventListener("pause", onPause, false);
 //    document.addEventListener("resume", onResume, false);
 //    
 }, false);
