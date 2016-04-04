beea6e768e9fdc7000263c04e733aa868ef2b704
Zaptronic <info@zaptronic.nl>
2016-04-04 18:11:21 +0200

    smalle changes to clear button

--- formfunctions.js
+++ formfunctions.js
@@ -16,11 +16,16 @@ function forminit() {
 function enterFormfield() {
     addsearchpage();
     document.activeElement.focus();
+    return false;
 }
 
 function clearPressed() {
     formCity.value(null);		
     addsearchpage();
+    return false;
+    if (formCity.value(null)) {
+        clearbutton.addClass('clearbutton__hide');
+    }
 }
 
 function keyPressed(event) {
